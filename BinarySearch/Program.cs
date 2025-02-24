4a776161870419284ef52d0a5e08b1bf78ebb757
MEGA <MEGA@gmail.com>
2025-02-24 18:51:27 +0200

    ZAD2

---
+++ BinarySearch/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            int a = int.Parse(Console.ReadLine());
+            int[] arr = new int[a];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = rand.Next(1, 11);
+            }
+            Array.Sort(arr);
+            int target = int.Parse(Console.ReadLine());
+            var result = BinarySearch(arr, target);
+            Console.WriteLine($"Намерени са {result.Count} елемента със стойност {target}.");
+            Console.WriteLine("Позиции на тези елементи:");
+
+            foreach (var position in result.Positions)
+            {
+                Console.WriteLine(position);
+            }
+        }
+        static SearchRESULT BinarySearch(int[] arr, int target)
+        {
+            List<int> positions = new List<int>();
+            int low = 0;
+            int high = arr.Length - 1;
+            int count = 0;
+
+            while (low <= high)
+            {
+                int mid = (low + high) / 2;
+
+                if (arr[mid] == target)
+                {
+                    positions.Add(mid);
+                    count++;
+
+                    int left = mid - 1;
+                    while (left >= 0 && arr[left] == target)
+                    {
+                        positions.Add(left);
+                        count++;
+                        left--;
+                    }
+
+                    int right = mid + 1;
+                    while (right < arr.Length && arr[right] == target)
+                    {
+                        positions.Add(right);
+                        count++;
+                        right++;
+                    }
+
+                    break;
+                }
+                else if (arr[mid] < target)
+                {
+                    low = mid + 1;
+                }
+                else
+                {
+                    high = mid - 1;
+                }
+            }
+            return new SearchRESULT(count, positions);
+        }
+    }
+}
---
+++ BinarySearch/SearchRESULT.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearch
+{
+    internal class SearchRESULT
+    {
+        public int Count { get; set; }
+        public List<int> Positions { get; set; }
+
+        public SearchRESULT(int count, List<int> positions)
+        {
+            Count = count;
+            Positions = positions;
+        }
+    }
+}
